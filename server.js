cc1bcb687225c02c66c8c5c387b9d8a10689d2b6
OLUBODUN IMRAN <olubodunalade@gmail.com>
2025-08-22 19:27:55 +0100

    last

--- server.js
+++ server.js
@@ -10,11 +10,10 @@ const port = process.env.PORT || 3000;
 dotenv.config();
 const { Pool } = pg;
 const db = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // required for Render
+  },
 });
 
 server.use(bodyParser.urlencoded({ extended: true }));
